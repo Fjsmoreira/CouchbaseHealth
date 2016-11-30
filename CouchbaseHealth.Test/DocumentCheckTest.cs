7bb1387714e39b275f88cf57d78f992dadbdbf08
fjsmoreira <fernandojsmoreira@gmail.com>
2016-11-30 08:28:32 +0100

    Added Test

---
+++ CouchbaseHealth.Test/DocumentCheckTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace CouchbaseHealth.Test
+{
+    public class DocumentCheckTest
+    {
+        [Fact]
+        public void ParameterToPass_IsNull_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(()=> new DocumentCheck(null));
+        }
+    }
+}
---
+++ CouchbaseHealth.Test/MemoryCheckTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Xunit;
+
+namespace CouchbaseHealth.Test
+{
+    public class MemoryCheckTest
+    {
+        [Fact]
+        public void ParameterToPass_IsNull_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new MemoryCheck(null));
+        }
+    }
+}
